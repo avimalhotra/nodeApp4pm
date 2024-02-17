2fb1ed26583a6817845901af3efc896ecca864f2
avimalhotra <avimalhotra505@gmail.com>
2024-02-17 17:08:59 +0530

    os and fs modules

--- src/app.js
+++ src/app.js
@@ -1,15 +1,73 @@
-//console.log(`Hello Node JS ${process.version}`);
-
-const ext=require("./ext");                               // cjs module
-//import {area} from './ext.js';                                    // esm module
-
-//require("colors");
-//const add=require("network-address");
+const os=require("os");
+const fs=require("fs");
 
 
-//console.log('app running'.yellow );
-//console.log('app running'.blue );
-//console.log('app running'.red );
-//console.log(add());
-//console.log(add.ipv6());
-console.log( ext );
+//const read=fs.readFileSync('src/data.txt');                   // avoid
+//console.log( read );                    // buffer
+//console.log( read.toString() );                    // string
+
+
+/* const read=fs.readFile("src/data.txt",{encoding:'utf-8'},(err,res)=>{
+    if(err){
+        console.warn(err);
+    }
+    else{
+        console.log(res); 
+    }
+});  */
+
+/* const read=fs.readFile("src/data.json",{encoding:'utf-8'},(err,res)=>{
+    if(err){
+        console.warn(err);
+    }
+    else{
+        console.log( JSON.parse(res) ); 
+    }
+});  */
+
+
+
+
+/* const read=fs.stat("src/app.txt",(err,res)=>{
+    if(err){
+        console.warn(err);
+    }
+    else{
+        console.log(res.size); 
+        console.log(res.isDirectory()); 
+        console.log(res.isFile()); 
+    }
+}); */
+
+/* fs.writeFile('src/data.txt',`file updated at ${new Date().toLocaleString()}`,'utf-8',(err)=>{
+    console.log(err);
+}); */
+
+/* fs.appendFile('src/data.txt',`file updated at ${new Date().toLocaleString()} \n`,'utf-8',(err)=>{
+    console.log(err);
+}); */
+
+/* fs.unlink("src/data.txt",(err,res)=>{
+    if(err){ console.log(err);}
+    else{ console.log(res);}
+}); */
+
+
+console.log("done");
+
+//console.log( os.cpus().length );
+//console.log( os.cpus()[0] );
+//os.cpus().forEach(i=>{console.log(i);})
+
+//console.log( os.freemem() );
+//console.log( os.totalmem() );
+
+//console.log( os.networkInterfaces() );
+
+
+//console.log(os.platform());
+//console.log(os.type());
+//console.log(os.uptime()/60/60);
+//console.log(os.userInfo());
+//console.log( os.machine() );
+
