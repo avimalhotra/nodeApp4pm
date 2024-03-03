43bf0463c6fbe59055e13a7fc833e60dafc24e8c
Avinash <avimalhotra505@gmail.com>
2024-03-03 21:22:45 +0530

    Express Get and Post

--- src/app.js
+++ src/app.js
@@ -2,22 +2,34 @@ const express=require("express");
 const app=express();
 require("dotenv").config();
 
-//app.use(express.static("src/public"));                  // define path of static resources
+app.use(express.static("src/public"));                  // define path of static resources
+app.use(express.static("node_modules/bootstrap/dist/")); // define path of static resources
 
 /* app.use((req,res,next)=>{
-    console.log(req.url, req.method);
+    console.log( "app");
     next();
-});
-app.use((req,res)=>{
+}); */
+
+/* app.use((req,res)=>{
     res.status=200;
     res.setHeader("Content-Type","text/html");
     res.end("<h1>Express JS</h1>");
-}); */
+});  */
 
 app.get("/",(req,res)=>{
     res.setHeader('Content-Type','text/html');
-    res.status(200).send(`<h1>Home Page</h1>`)
+    res.status(200).send(`<h1>Home Page, ${req.url}</h1>`);
 });
+app.get("/search",(req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    //res.status(200).send(req.query);
+    res.status(200).json(req.query);
+});
+app.get("/:brand/:product/:model",(req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.status(200).send(req.params);
+});
+
 app.post("/login",(req,res)=>{
     res.status(200).send(`Post data`);
 });
@@ -26,7 +38,6 @@ app.get("/app",(req,res)=>{
     res.setHeader('Content-Type','text/html');
     res.status(200).send(`<h1>App Page</h1>`)
 });
-
 
 
 
