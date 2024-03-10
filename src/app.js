d77c0b83c42f0fa5a679a8bc835d3118c867bc6a
Avinash <avimalhotra505@gmail.com>
2024-03-10 17:03:48 +0530

    Express Session and Web API

--- src/app.js
+++ src/app.js
@@ -3,13 +3,24 @@ const app=express();
 require("dotenv").config();
 const bp=require("body-parser");
 const cookieparser=require('cookie-parser');
+const session=require("express-session");
+const parseurl=require("parseurl");
+
 
 app.use(bp.urlencoded({ extended: false })); 
-app.use(cookieparser("secret"));
-//app.use(bp.json());
+app.use(cookieparser());
 
-//app.use(express.static("src/public"));                  // define path of static resources
-//app.use(express.static("node_modules/bootstrap/dist/")); // define path of static resources
+/* app.set("trust proxy",1);
+app.use(session({
+    secret:"session",
+    resave:false,
+    saveUninitialized:true,
+    cookie:{secure:false,maxAge:300000}
+})); */
+app.use(bp.text());
+
+app.use(express.static("src/public"));                  // define path of static resources
+app.use(express.static("node_modules/bootstrap/dist/")); // define path of static resources
 
 
 /* routes */
@@ -22,9 +33,16 @@ app.use("/user",user);
 
 /* middleware */
 /* app.use((req,res,next)=>{
-    console.log( "app");
+    if (!req.session.views) { req.session.views = {}}
+    
+      // get the url pathname
+      var pathname = parseurl(req).pathname
+    
+      // count the views
+      req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+    
     next();
-}); */
+});  */
 
 /* app.use((req,res)=>{
     res.status=200;
@@ -32,16 +50,33 @@ app.use("/user",user);
     res.end("<h1>Express JS</h1>");
 });  */
 
+const data=["jan","feb","mar","apr","may","jun"];
+
 app.get("/",(req,res)=>{
     //console.log(req.cookies);
     //res.cookie("pin","201301",{signed:true});
     //res.cookie("state","up", {maxAge:86400000, httpOnly: true});
-    if(req.cookies.name){ console.log(req.cookies.name)}
-    else{ console.log("No name defined")}
+    //if(req.cookies.name){ console.log(req.cookies.name)}
+   // else{ console.log("No name defined")}
     
     res.setHeader('Content-Type','text/html');
-    res.status(200).send(`<h1>Hello ${req.signedCookies.pin}</h1>`);
+    res.status(200).send(`<h1> Express session</h1>`);
 });
+
+
+/* REST API */
+app.get("/api",(req,res)=>{
+    res.header('Access-Control-Allow-Origin',"*");
+    return res.status(200).send(data);
+});
+
+app.post("/postapi",(req,res)=>{    
+    const x=JSON.parse(req.body);
+    const t=data.filter(i=>i==x.query);
+    return res.status(200).send(t);
+});
+
+
 app.get("/search",(req,res)=>{
     res.setHeader('Content-Type','text/html');
     //res.status(200).send(req.query);
