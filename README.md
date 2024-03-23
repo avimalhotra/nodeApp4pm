b2c6e71d6c001d91a1ec6ead44b0f40c01d06ce5
Avinash <avimalhotra505@gmail.com>
2024-03-23 19:52:14 +0530

    Nunjucks

--- README.md
+++
@@ -1,3 +0,0 @@
-NODE JS Class 4 pm 
-
-use "type":"module" for ES6 Modules in package.json file
--- src/app.js
+++ src/app.js
@@ -1,143 +1,43 @@
-const express=require("express");
-const app=express();
-const path=require("path");
 require("dotenv").config();
-const bp=require("body-parser");
-const cookieparser=require('cookie-parser');
-const session=require("express-session");
-const parseurl=require("parseurl");
+const port=process.env.port || 3000;
+const path=require("path");
+const express=require("express")
+const app=express();
+const nunjucks=require("nunjucks");
 
-const ejs=require("ejs");
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public/views'));
+//const year=2024;
+const car={name:"swift",power:90};
+const arr=[2,3,4,8];
 
-//const LRU=require("lru-cache");
-//ejs.cache = LRU(100);
 
-const multer=require("multer");
-//const upload=multer({dest:"src/public/uploads/"});
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'src/public/uploads/')
-    },
-    filename: function (req, file, cb) {
-        //cb(null, (file.originalname)) ;
-      cb(null, Date.now() + path.extname(file.originalname)) 
-    }
+// configure
+nunjucks.configure(path.resolve(__dirname,'public/views'),{
+    express:app,
+    autoscape:true,
+    noCache:false,
+    watch:true
 });
-const upload=multer({storage:storage});
 
-
-
-app.use(bp.urlencoded({ extended: false })); 
-app.use(cookieparser());
-
-/* app.set("trust proxy",1);
-app.use(session({
-    secret:"session",
-    resave:false,
-    saveUninitialized:true,
-    cookie:{secure:false,maxAge:300000}
-})); */
-app.use(bp.text());
-
-app.use(express.static("src/public"));                  // define path of static resources
-app.use(express.static("node_modules/bootstrap/dist/")); // define path of static resources
-
-
-/* routes */
-const admin=require("./routes/admin");
-const user=require("./routes/user");
-
-
-app.use("/admin",admin);
-app.use("/user",user);
-
-/* middleware */
-/* app.use((req,res,next)=>{
-    if (!req.session.views) { req.session.views = {}}
-    
-      // get the url pathname
-      var pathname = parseurl(req).pathname
-    
-      // count the views
-      req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-    
-    next();
-});  */
-
-/* app.use((req,res)=>{
-    res.status=200;
-    res.setHeader("Content-Type","text/html");
-    res.end("<h1>Express JS</h1>");
-});  */
-
-const data=["jan","feb","mar","apr","may","jun"];
+app.use(express.static('src/public'));
 
 app.get("/",(req,res)=>{
-    //console.log(req.cookies);
-    //res.cookie("pin","201301",{signed:true});
-    //res.cookie("state","up", {maxAge:86400000, httpOnly: true});
-    //if(req.cookies.name){ console.log(req.cookies.name)}
-   // else{ console.log("No name defined")}
-    
-    res.setHeader('Content-Type','text/html');
-    //res.status(200).send(`<h1> Express session</h1>`);
-    res.render('index',{h1:"EJS Website",title:"Hello World",user:{name:"Avinash",id:200},data:["jan","feb","mar","apr"]});
-});
-app.get("/about",(req,res)=>{
-    res.render('about',{h1:"About US",title:"About Us"});
-});
-app.get("/contact",(req,res)=>{
-    res.render('contact',{h1:"Contact US"});
+    res.status(200).render("index.html",{ 
+        title:"express app", 
+        car:car, 
+        sayHi(){ return 2+3 }, 
+        data:arr,
+        id:35,
+    });
 });
 
-/* REST API */
-app.get("/api",(req,res)=>{
-    res.header('Access-Control-Allow-Origin',"*");
-    return res.status(200).send(data);
-});
-
-app.post("/postapi",(req,res)=>{    
-    const x=JSON.parse(req.body);
-    const t=data.filter(i=>i==x.query);
-    return res.status(200).send(t);
-});
+app.get("/about",(req,res)=>{res.status(200).render("about.html",{ title:"About Us"})});
+app.get("/contact",(req,res)=>{res.status(200).render("contact.html",{ title:"Contact Us"})});
 
 
-app.get("/search",(req,res)=>{
-    res.setHeader('Content-Type','text/html');
-    //res.status(200).send(req.query);
-    res.status(200).json(req.query);
-});
-app.get("/:brand/:product/:model",(req,res)=>{
-    res.setHeader('Content-Type','text/html');
-    res.status(200).send(req.params);
-});
-
-app.post("/login",(req,res)=>{
-    //for(let i in req.body){ console.log(i, req.body[i]);}
-    res.status(200).json(`${req.body.email} ${req.body.pass}, Post data`);
-});
-
-app.get("/app",(req,res)=>{
-    res.setHeader('Content-Type','text/html');
-    res.status(200).send(`<h1>App Page</h1>`);
-});
-app.post("/upload",upload.single("picture"),(req,res)=>{
-    console.log(req.file);
-    //console.log(req.body);
-    
-    res.status(200).send("File Uploaded");
-});
-
-
-/* wild card handler */
 app.get("/**",(req,res)=>{
-    res.setHeader('Content-Type','text/html');
-    res.status(404).send(`<h1>404, Page Not Found</h1>`)
-});
+    res.status(404).render("error.html",{title:"404"})
+})
 
-app.listen(process.env.PORT,()=>{
-    console.log(`App server running at http://127.0.0.1:${process.env.PORT}`);
-});
+app.listen(port,()=>{
+    console.log(`App running at http://127.0.0.1:${port}`);
+})
